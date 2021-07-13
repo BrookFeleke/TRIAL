906f0f908a6a51ffa32965824ee81f71ba39f3fb
Brook <felekebrook05@gmail.com>
2021-07-13 23:53:16 +0300

    first time

---
+++ NUMBERGAME1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(){
+    cout<<" ********THE NUMBER GAME ********"<<endl;
+int player;
+int numg[4], arrb;
+int num[4];
+int arrb1 = rand()%10 +7;
+arrb = rand()%10+arrb1;
+for( int i = 0; i<= arrb; i++){
+
+    a:num[0]= rand()%10;
+    b:num[1]= rand()%10;
+    if( num[1]== num[0])
+        goto b;
+    c:num[2]= rand()%10;
+    if(num[2]== num[0] || num[2]==num[1] )
+        goto c;
+    d:num[3]= rand()%10;
+    if( num[3]== num[0] || num[3]==num[1] || num[3]==num[2])
+        goto d;
+}
+
+int digit, pcount = 0, ncount = 0;
+int G = 1;
+while(G){
+pcount = 0;
+ncount = 0;
+    cout<< "\n input non repeating numbers:";
+    cin>>player;
+for( int i = 3; i>=0; i-- ){
+    digit = player%10;
+    numg[i] = digit;
+    player = player/10;
+
+for( int j = 0; j<4; j++){
+        if( numg[i]==num[j]){
+            ncount++;
+            if( i == j){
+                pcount++;}
+        }
+       }
+}
+cout<< " numbers : "<<ncount<<"     positions : "<<pcount<<endl;
+ if(pcount == 4){
+    G = 0;}
+}
+cout<<" ******You won****** "<<endl;
+
+system("pause");
+return main();
+}
